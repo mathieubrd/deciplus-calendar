da01b3cc6ea846c543ec44d39efe1f8786a0f7d9
Mathieu Brochard <mathieu.brochard@outlook.com>
2021-08-14 15:41:22 +0200

    feat: add deciplus service

---
+++ src/services/deciplus.ts
@@ -0,0 +1,95 @@
+import fetch from 'node-fetch'
+import Booking from '../models/booking'
+
+export type DeciplusSession = {
+	token: string
+}
+
+type DeciplusSignInResponse = {
+	tokens: {
+		clubs: {
+			[key: string]: [{
+				token: string
+			}]
+		}
+	}
+}
+
+type DeciplusBookingsResponse = {
+	bookings: [{
+		booking: {
+			startDate: string
+			activity: {
+				name: string
+			}
+		}
+	}]
+} 
+
+const baseUrl = 'https://api.deciplus.pro/deciplus-members/v1'
+const baseUrlMembers = 'https://api.deciplus.pro/members/v1'
+const signInUrl = `${baseUrl}/authenticate`
+const bookingsUrl = `${baseUrlMembers}/bookings/upcoming`
+
+type RequestParameters = {
+	url: string,
+	method: string,
+	session?: DeciplusSession,
+	body?: string
+}
+
+const request = async <T>({ url, method, session, body }: RequestParameters): Promise<T> => {	
+	const response = await fetch(url, {
+		method,
+		body,
+		headers: {
+			'Content-Type': 'application/json',
+			...(session && { 'x-access-token': session.token })
+		}
+	})
+
+	if (response.status !== 200) {
+		throw new Error(await response.json())
+	}
+
+	return await response.json() as T 
+}
+
+const signIn = async (email: string, password: string): Promise<DeciplusSession> => {
+	const response = await request<DeciplusSignInResponse>({
+		url: signInUrl,
+		method: 'POST',
+		body: JSON.stringify({
+			email,
+			password
+		})
+	})
+
+	const session: DeciplusSession = {
+		token: response.tokens.clubs.cfstsimon[0].token
+	}
+
+	return session
+}
+
+const getBookings = async (session: DeciplusSession): Promise<Booking[]> => {
+	const response = await request<DeciplusBookingsResponse>({
+		url: bookingsUrl,
+		session,
+		method: 'GET'
+	})
+
+	return response.bookings.map(({booking}): Booking => {
+		return {
+			activity: booking.activity.name,
+			date: new Date(booking.startDate)
+		}
+	})
+}
+
+const DeciplusClient = {
+	signIn,
+	getBookings
+}
+
+export default DeciplusClient
