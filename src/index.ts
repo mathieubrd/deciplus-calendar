4a6c986c0c750357d77735a334c8ab22c7188150
Mathieu Brochard <mathieu.brochard@outlook.com>
2021-08-15 12:15:45 +0200

    feat: pass deciplus credentials as environment variables

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+import { config as dotenv } from 'dotenv'
+
+dotenv()
+
 export const handler = () => {
-	console.log('dummy')
+	if (!process.env.DECIPLUS_EMAIL || !process.env.DECIPLUS_PASSWORD) {
+		throw new Error('Deciplus credentials must be provided as env variables')
+	}
 }
