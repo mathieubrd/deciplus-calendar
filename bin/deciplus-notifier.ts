0d2dc794e233f1ee0595d119aa776d3d05db6ff6
Mathieu Brochard <mathieu.brochard@outlook.com>
2021-09-04 16:22:27 +0200

    refactor: rename projet to deciplus-notifier

--- bin/deciplus-notifier.ts
+++ bin/deciplus-notifier.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { DeciplusCalendarStack } from '../lib/deciplus-calendar-stack';
+import { DeciplusNotifierStack } from '../lib/deciplus-notifier-stack';
 
 const app = new cdk.App();
-new DeciplusCalendarStack(app, 'DeciplusCalendarStack', {
+new DeciplusNotifierStack(app, 'DeciplusNotifierStack', {
   stackName: process.env.STACK_NAME
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
--- lib/deciplus-notifier-stack.ts
+++ lib/deciplus-notifier-stack.ts
@@ -1,11 +1,11 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda-nodejs'
 
-export class DeciplusCalendarStack extends cdk.Stack {
+export class DeciplusNotifierStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new lambda.NodejsFunction(this, 'DeciplusCalendarFunction', {
+    new lambda.NodejsFunction(this, 'DeciplusNotifierFunction', {
       entry: 'src/index.ts'
     })
   }
